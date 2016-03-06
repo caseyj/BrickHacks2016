e88eea1c8a79f3000ce4b1a44af405c40436616a
vms3476 <vms3476@rit.edu>
2016-03-06 02:36:30 +0000

    created input method for main file

--- main.py
+++ main.py
@@ -3,6 +3,19 @@
 ##
 ##############
 
+import json
+
+
 def main():
+    #take in name of json file from CL
+    fileName = sys.argv[0]
+    
+    with open(fileName) as json_file:
+        json_data = json.load(json_file)
+    
+    # preference first
+    # apply all dietary needs and allergies to that 
+    
+    
     
 if __name__ == "__main__": main()
