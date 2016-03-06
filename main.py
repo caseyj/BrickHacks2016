a246d267fc507d8aeab97367d54cc658f0af0fdb
vms3476 <vms3476@rit.edu>
2016-03-06 08:17:43 +0000

    some comments added including flow of the next few additions

--- main.py
+++ main.py
@@ -5,24 +5,27 @@
 
 import json
 import sys
+from guest import *
 
 def main():
     #take in name of json file from CL
     fileName = sys.argv[1]
-    print fileName
+    
+    #create an empty list for all of our guests
     guests = list()
     
+    #opens the json file and saves to variable json_data
     with open(fileName, 'r') as json_file:
         json_data = json.load(json_file)
-    
-    guests = list
+
+    #loops through each object in the json and creates a guest for each one
+    #   each guest is stored in guests
     for i in json_data:
-        print i['pref']
+        g = Guest(i['ID'], i['name'], i['pref'], i['diet'], i['allergy'], i['exclude'])
+        guests.append(g)
     
+    #here we will make a call to grouper
     
-    # preference first
-    # apply all dietary needs and allergies to that 
-    
-    
+    #here we will output our grouper results to json&return
     
 if __name__ == "__main__": main()
