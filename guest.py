5b80398b05b7c583782760af3655debe6d451285
vms3476 <vms3476@rit.edu>
2016-03-06 02:57:12 +0000

    progress

--- guest.py
+++ guest.py
@@ -2,6 +2,8 @@ import numpy
 
 class Guest(object):
 
+	preferences = numpy.asarray( ['Beef','Chicken','Plant based','Pork','Seafood'] )
+
 	diets = numpy.asarray( ['Pescetarian','Lacto-ovo vegetarian', 'Lacto vegetarian', 'Ovo vegetaian', 'Vegan', 'Paleo'] )
 
 	allergies = numpy.asarray( ['Sesame-Free','Seafood-Free','Tree Nut-Free', \
@@ -51,7 +53,7 @@ class Guest(object):
 		return self._pref
 
 	@pref.setter
-	def pref(self, name):
+	def pref(self, pref):
 		self._pref = pref
 
 	@pref.deleter
@@ -65,7 +67,7 @@ class Guest(object):
 		return self._diet
 
 	@diet.setter
-	def diet(self, name):
+	def diet(self, diet):
 		self._diet = diet
 
 	@diet.deleter
@@ -91,7 +93,7 @@ class Guest(object):
 		return self._allergy
 
 	@allergy.setter
-	def allergy(self, name):
+	def allergy(self, allergy):
 		self._allergy = allergy
 
 	@allergy.deleter
@@ -117,7 +119,7 @@ class Guest(object):
 		return self._exclude
 
 	@exclude.setter
-	def exclude(self, name):
+	def exclude(self, exclude):
 		self._exclude = exclude
 
 	@exclude.deleter
@@ -132,7 +134,12 @@ class Guest(object):
 
 
 if __name__ == '__main__':
-
+	# beef, chicken, plant based, pork, seafood
+	vic_pref = numpy.asarray([0,0,1,0,0])
+	john_pref = numpy.asarray([0,0,0,1,0])
+	victoria_pref = numpy.asarray([0,1,0,0,0])
+	danny_pref = numpy.asarray([1,0,0,0,0])
+	
 	# pescetarian, vegetarian, laco vegetarian, ovo vegetarian, vegan, paleo
 	vic_diet = numpy.asarray( [0,0,0,0,1,0] )
 	john_diet = numpy.asarray( [0,0,0,0,0,0] )
@@ -150,10 +157,10 @@ if __name__ == '__main__':
 	victoria_exclude = numpy.asarray( [''] )
 	danny_exclude = numpy.asarray( ['Tomato'] )
 
-	vic = Guest( '9999', 'Vic', 'Tofu', vic_diet, vic_allergy, vic_exclude )
-	john = Guest( '8675', 'John', 'Pork', john_diet, john_allergy, john_exclude)
-	victoria = Guest( '1234', 'Victoria', 'Chicken', victoria_diet, victoria_allergy, victoria_exclude )
-	danny = Guest( '8881', 'Danny', 'Beef', danny_diet, danny_allergy, danny_exclude )
+	vic = Guest( '9999', 'Vic', vic_pref, vic_diet, vic_allergy, vic_exclude )
+	john = Guest( '8675', 'John', john_pref, john_diet, john_allergy, john_exclude)
+	victoria = Guest( '1234', 'Victoria', victoria_pref, victoria_diet, victoria_allergy, victoria_exclude )
+	danny = Guest( '8881', 'Danny', danny_pref, danny_diet, danny_allergy, danny_exclude )
 
 	print vic.ID, vic.name, vic.pref, vic.allergy, vic.diet, vic.exclude
 	print john.ID, john.name, john.pref, john.allergy, john.diet, john.exclude
--- main.py
+++ main.py
@@ -10,8 +10,14 @@ def main():
     #take in name of json file from CL
     fileName = sys.argv[0]
     
+    guests = list()
+    
     with open(fileName) as json_file:
         json_data = json.load(json_file)
+    
+    for i in range(json_data):
+        print json_data[i]['']
+    
     
     # preference first
     # apply all dietary needs and allergies to that 
